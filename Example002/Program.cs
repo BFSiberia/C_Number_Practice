f36cb560f7a0916f07366deaee4e2ea797ffa372
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-10 16:25:01 +0800

    add Example002 part 2

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,11 +1,21 @@
-﻿int a = -3; 
-int b = 10;
-if(b == a*a)
-{
-Console.WriteLine("Да");
-}
-else
-{
-Console.WriteLine("Нет");
-}
+﻿//int a = -3, b = 9;
+//if(b == a*a)
+//{
+//Console.WriteLine("Да");
+//}
+//else
+//{
+//Console.WriteLine("Нет");
+//}
 
+Console.WriteLine("Введите номер дня недели: ");
+int weekDay = 5;
+
+if (weekDay == 3)
+{
+Console.WriteLine("Это среда");
+}
+if (weekDay ==5)
+{
+Console.WriteLine("Это пятница");
+}

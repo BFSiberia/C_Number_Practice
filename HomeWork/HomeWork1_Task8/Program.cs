e400c115455a6dd2246c5caa750b4f46b9cb503f
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-11 14:38:08 +0800

    add HomeWork1_Task8

---
+++ HomeWork/HomeWork1_Task8/Program.cs
@@ -0,0 +1,22 @@
+﻿int number, i;
+
+number = 5;
+i = 1;
+
+while (i <= number)
+{
+    if (i %2 == 0)
+        Console.Write($"{i} ");
+i++;
+}
+Console.WriteLine();
+
+number = 8;
+i = 1;
+
+while (i <= number)
+{
+    if (i %2 == 0)
+        Console.Write($"{i} ");
+i++;
+}

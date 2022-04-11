299c1136896eba1bb823344d715644770ed89994
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-11 14:16:31 +0800

    add HomeWork1_Task6

---
+++ HomeWork/HomeWork1_Task6/Program.cs
@@ -0,0 +1,30 @@
+﻿int num;
+num =4;
+if (num %2 ==0)
+{
+Console.WriteLine($"Число {num} четное.");
+}
+else
+{
+Console.WriteLine($"Число {num} нечетное.");
+}
+
+num =-3;
+if (num %2 ==0)
+{
+Console.WriteLine($"Число {num} четное.");
+}
+else
+{
+Console.WriteLine($"Число {num} нечетное.");
+}
+
+num =7;
+if (num %2 ==0)
+{
+Console.WriteLine($"Число {num} четное.");
+}
+else
+{
+Console.WriteLine($"Число {num} нечетное.");
+}

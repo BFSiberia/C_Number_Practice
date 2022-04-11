0daac22bf96b9202029308344fd4985cac32e32a
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-11 12:26:54 +0800

    add HomeWork and HomeWork1_Task2

---
+++ HomeWork/HomeWork1_Task2/Program.cs
@@ -0,0 +1,61 @@
+﻿int a, b, max;
+a = 5;
+b = 7;
+max = a;
+
+if(a==b)
+{
+Console.WriteLine("Числа a и b равны.");
+}
+else
+{
+    if (b>a)
+    {
+    max = b;
+    Console.WriteLine($"Число b ({b}) больше числа a ({a}).");
+    }
+    else
+    {
+    Console.WriteLine($"Число a ({a}) больше числа b ({b}).");  
+    }
+}
+a = 2;
+b = 10;
+max = a;
+
+if(a==b)
+{
+Console.WriteLine("Числа a и b равны.");
+}
+else
+{
+    if (b>a)
+    {
+    max = b;
+    Console.WriteLine($"Число b ({b}) больше числа a ({a}).");
+    }
+    else
+    {
+    Console.WriteLine($"Число a ({a}) больше числа b ({b}).");  
+    }
+}
+a = -9;
+b = -3;
+max = a;
+
+if(a==b)
+{
+Console.WriteLine("Числа a и b равны.");
+}
+else
+{
+    if (b>a)
+    {
+    max = b;
+    Console.WriteLine($"Число b ({b}) больше числа a ({a}).");
+    }
+    else
+    {
+    Console.WriteLine($"Число a ({a}) больше числа b ({b}).");  
+    }
+}

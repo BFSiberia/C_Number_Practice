401c143d01ae21586620cd04ebf999cf6d815a0b
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-15 13:00:38 +0800

    add Example002 weekdays

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,21 +1,20 @@
-﻿//int a = -3, b = 9;
-//if(b == a*a)
-//{
-//Console.WriteLine("Да");
-//}
-//else
-//{
-//Console.WriteLine("Нет");
-//}
-
-Console.WriteLine("Введите номер дня недели: ");
-int weekDay = 5;
-
-if (weekDay == 3)
+﻿int a = -3, b = 9;
+if(b == a*a)
 {
-Console.WriteLine("Это среда");
+Console.WriteLine("Да");
 }
-if (weekDay ==5)
+else
 {
-Console.WriteLine("Это пятница");
+Console.WriteLine("Нет");
 }
+
+//Console.WriteLine("Введите номер дня недели: ");
+int weekDay = 3;
+
+if (weekDay == 1) Console.WriteLine("Это понедельник");
+if (weekDay == 2) Console.WriteLine("Это вторник");
+if (weekDay == 3) Console.WriteLine("Это среда");
+if (weekDay == 4) Console.WriteLine("Это четверг");
+if (weekDay == 5) Console.WriteLine("Это пятница");
+if (weekDay == 6) Console.WriteLine("Это суббота");
+if (weekDay == 7) Console.WriteLine("Это воскресенье");

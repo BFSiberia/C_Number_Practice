fbcca4d1b7c87243ea1b7e9c0bdb43de5f1e28bc
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-17 21:30:09 +0800

    replacement

---
+++ Examples/Example003/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int number, i;
+
+number = 0;
+i = -number;
+
+while (i <= number)
+{
+    if (i == number)
+        Console.Write($"{i}. ");
+    else
+        Console.Write($"{i}, ");
+i++;
+}
+
+int num = 782;
+num %= 10; // number = number % 10;
+Console.WriteLine(num);
---
+++ Examples/Example005/Program.cs
@@ -0,0 +1,31 @@
+﻿int x,y,section;
+
+Console.WriteLine("Введите точку по оси x: ");
+x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите точку по оси y: ");
+y = int.Parse(Console.ReadLine());
+
+while(x==0^ y==0)
+{
+    Console.WriteLine("Точка координат не может равняться 0: ");
+    Console.WriteLine("Введите точку по оси x: ");
+    x = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите точку по оси y: ");
+    y = int.Parse(Console.ReadLine());
+}
+if(x>0&y>0) 
+{section =1;
+Console.WriteLine($"Номер четверти {section}");
+}
+if(x>0&y<0) 
+{section =2;
+Console.WriteLine($"Номер четверти {section}");
+}
+if(x<0&y<0)
+{section =3;
+Console.WriteLine($"Номер четверти {section}");
+}
+if(x<0&y>0) 
+{section =4;
+Console.WriteLine($"Номер четверти {section}");
+}

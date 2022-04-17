3487b06dfc47080526174189abfcfa0b500f98fd
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-17 16:51:40 +0800

    add Example007

---
+++ Example007/Program.cs
@@ -0,0 +1,9 @@
+﻿// int[] coordinates = {1,2};
+
+int coordinates = x1, x2, y1,y2;
+x1 = new Random().Next(-10,10);
+x2 = new Random().Next(-10,10);
+y1 = new Random().Next(-10,10);
+y2 = new Random().Next(-10,10);
+double distance = Math.Sqrt(Math.Pow(x1-x2,2)+ Math.Pow(y1-y2,2));
+Console.WriteLine($"Растояние между точками: {distance}.");

c736ccef11fbc8c270970194c71005eb119ef45b
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-17 16:03:35 +0800

    add amendments Example006

--- Example006/Program.cs
+++ Example006/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Write("Введите номер четверти от 1 до 4: ");
 int section = int.Parse(Console.ReadLine());
 
-if (section == 1) Console.WriteLine("Диапазон координат x от 1 до +бесконечности, y от 1 до +бесконечности.");
-else if (section == 2) Console.WriteLine("Диапазон координат x от 1 до +бесконечности, y от -1 до -бесконечности.");
-else if (section == 3) Console.WriteLine("Диапазон координат x от -1 до -бесконечности, y от -1 до -бесконечности.");
-else if (section == 4) Console.WriteLine("Диапазон координат x от 1 до +бесконечности, y от 1 до -бесконечности.");
+if (section == 1) Console.WriteLine("Диапазон координат x>0, y>0.");
+else if (section == 2) Console.WriteLine("Диапазон координат x>0, y<0.");
+else if (section == 3) Console.WriteLine("Диапазон координат x<0, y<0.");
+else if (section == 4) Console.WriteLine("Диапазон координат x>0, y<0.");
 else if (section >=0 & section<5) Console.WriteLine("Номер четверти должен быть в диапазоне от 1 до 4.");

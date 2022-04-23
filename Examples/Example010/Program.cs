afb2a334fced6bca480a16bfa0576d72b0512c2a
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-23 16:34:04 +0800

    add Example010

---
+++ Examples/Example010/Program.cs
@@ -0,0 +1,17 @@
+﻿// выдает количество цифр в числе
+
+int N = new Random().Next(1,1000);
+
+int Qty(int N)
+{
+    int length = 0;
+    for(int i=1;N>0;i++)
+    {
+    N = N/10;
+    length += 1;
+    }
+return length;
+}
+Console.WriteLine(N);
+Console.WriteLine(Qty(N));
+

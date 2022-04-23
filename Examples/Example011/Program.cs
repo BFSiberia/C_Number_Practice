1eb977d1fbd26e3f0d7fa94f638168903c12e6f2
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-23 16:44:47 +0800

    add Example011

---
+++ Examples/Example011/Program.cs
@@ -0,0 +1,21 @@
+﻿//массив из 0 и 1 случайным образом 8 элементов
+void FillArray(int[] array)
+{
+    for(int i =0;i<array.Length;i++)
+    {
+        array[i]= new Random().Next(0,2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for(int i =0;i<array.Length;i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);

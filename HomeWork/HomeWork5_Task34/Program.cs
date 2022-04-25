2b2e3b73ffd7e0c15c0b1de7e00c634d27b65d94
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-25 23:35:10 +0800

    add HomeWork5 Task34

---
+++ HomeWork/HomeWork5_Task34/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void CreateArray(int[] array)
+{
+    for(int i = 0; i<array.Length;i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i<array.Length;i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void CountEven(int[] array)
+{
+int count = 0;
+for(int i =0;i<array.Length;i++)
+{   
+    if (array[i]%2==0) count++;
+}
+Console.WriteLine("Количество четных числе в массиве: " + count);
+}
+
+int[] array = new int [7];
+
+CreateArray(array);
+PrintArray(array);
+CountEven(array);

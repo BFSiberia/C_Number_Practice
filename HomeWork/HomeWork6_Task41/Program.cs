fd2b325293fa1d9462ea3d6f5942870885ea4805
Ольга Смолина <a.v.tarasov@gmail.com>
2022-05-03 20:51:48 +0800

    add HomeWork6_Task41

---
+++ HomeWork/HomeWork6_Task41/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввел пользователь.
+
+Console.WriteLine("Введите числа через пробел, по зверешению нажмите Ввод: ");
+string list = Console.ReadLine();
+string[] array = list.Split(" ");
+
+int count =0;
+for (int i = 0; i<array.Length; i++)
+{
+if (int.Parse(array[i]) >0) count = count +1;
+}
+Console.WriteLine("Количество чисел больше 0: "+count);

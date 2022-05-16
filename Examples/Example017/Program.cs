16dd81dff65836c633f1412d72373e4fa7e6b055
Ольга Смолина <a.v.tarasov@gmail.com>
2022-05-17 03:59:06 +0800

    amend Example017

--- Examples/Example017/Program.cs
+++ Examples/Example017/Program.cs
@@ -1,15 +1,37 @@
 ﻿// Задайте двумерный массив. Напишите программу,которая поменяет местами 
 //первую и последнюю строку массива.
-void PrintArray(int[,] array)
+int[,] FillArrayDouble(int m, int n)
 {
-for (int i=0;i<array.GetLength(0);i++)
-{
-    for (int j=0;j<array.GetLength(1);j++)
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        Console.Write($" {array[i,j]}");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArrayDouble(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArraySingle(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]}");
     }
     Console.WriteLine();
-}
 }
 
 // int[,] array =
@@ -26,7 +48,7 @@ for (int i=0;i<array.GetLength(0);i++)
 //         array[array.GetLength(0)-1,j] = temp;
 //     }
 
-// PrintArray(array);
+// PrintArrayDouble(array);
 
 
 // Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
@@ -47,75 +69,101 @@ for (int i=0;i<array.GetLength(0);i++)
 //     int[,] matrix1= new int[matrix.GetLength(0),matrix.GetLength(1)];
 //     for (int i=0;i<matrix1.GetLength(0);i++)
 // {
-//     for (int j=0;j<matrix1.GetLength(1);j++) // int j=i для смещения по диаганали
+//     for (int j=0;j<matrix1.GetLength(1);j++) // int j=i для смещения по диагонали
 //     {
 //         matrix1[i,j] = matrix[j,i];
 //     }
 // }
-// PrintArray(matrix1);
+// PrintArrayDouble(matrix1);
 // };
 
-// Составить частотный словарь элементов двумерного массива. Частотный словарь содержит
-//информацию о том, сколько раз встречается элемент входных данных.
+// Задача 57. Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит  информацию о том, сколько раз 
+// встречается элемент входных данных.
 
-// int[] array = 
-// {1, 9, 9, 0, 2, 8, 0, 9};
+int[] arraySingle =
+{1, 9, 9, 0, 2, 8, 0, 9};
 
-// int count =0;
-// int[] unique = new int [1];
-// unique[0] = array[0];
-
-// for (int j=0;j<unique.Length;j++)
+int[,] arrayDouble = FillArrayDouble(3,3);
 // {
-//     for (int i=0; i<array.Length;i++)
-//     {
-//     if (array[i] == unique[j])
-//     {
-//         count = count +1;
-//     }
-//     }
-//     Console.WriteLine($"Количество элементов {unique[j]}: {count}");
-// }
+//     {1, 2, 3},
+//     {4, 6, 1},
+//     {2, 1, 6}
+// };
+
+int[,] unique = new int[arrayDouble.Length, 2];
+int count = 0;
+bool exist = false; // переменная для проверки вхождения в новый массив
+
+for (int i = 0; i < arrayDouble.GetLength(0); i++)  //проход по строкам исходного массива
+{
+    for (int j = 0; j < arrayDouble.GetLength(1); j++)  // проход по столбцам
+    {
+        exist = false;             // сброс проверки вхождения
+        for (int c = 0; c < count; c++) // цикл для проверки вхождения уникального элемента
+        {
+            if (arrayDouble[i, j] == unique[c, 0])        // проверка вхождения
+            {
+                exist = true;
+                unique[c, 1]++;                     // счетчик уникального числа
+                break;                              //остановка цикла
+            }
+        }
+        if (exist == false)                     // если нет вхождения
+        {
+            unique[count, 0] = arrayDouble[i, j];  // элемент входит в уникальный массив 
+            unique[count, 1] = 1;       // начало счета уникального числа
+            count++;                  // сдвиг на пустой элемент уник. массива
+        }
+    }
+}
+
+PrintArrayDouble(unique);
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine($"Цифра {unique[i, 0]} встречается {unique[i, 1]} раз.");
+}
 
 // Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на
 // пересечении которых расположен наименьший элемент массива.
 
-int [,] array =
-{
-    {1,4,5,2},
-    {5,9,2,3},
-    {8,4,2,4},
-    {5,2,6,7},
-};
+// int [,] array =
+// {
+//     {1,4,5,2},
+//     {5,9,2,3},
+//     {8,4,2,4},
+//     {5,2,6,7},
+// };
 
-int min = array[0,0];
-int row = 0;
-int colomn = 0;
+// int min = array[0,0];
+// int row = 0;
+// int colomn = 0;
 
-for (int i=0;i<array.GetLength(0);i++)
-{
-    for (int j=0;j<array.GetLength(1);j++)
-    {
-        if (array[i,j]<min)
-        min = array[i,j];
-        row = i;
-        colomn =j;
-    }
-}
+// for (int i=0;i<array.GetLength(0);i++)
+// {
+//     for (int j=0;j<array.GetLength(1);j++)
+//     {
+//         if (array[i,j]<min)
+//         min = array[i,j];
+//         row = i;
+//         colomn =j;
+//     }
+// }
 
-int [,] newarray = new int[array.GetLength(0)-1,array.GetLength(1)-1];
+// int [,] newarray = new int[array.GetLength(0)-1,array.GetLength(1)-1];
 
-for (int i=0;i<newarray.GetLength(0);i++)
-{
-    for (int j=0;j<newarray.GetLength(1);j++)
-    {
-        
-        if (i == row ^ j ==colomn)
-        newarray[i,j] = array[i+1,j+1];
-        else
-        newarray[i,j] = array[i,j];
-    }
-}
+// for (int i=0;i<newarray.GetLength(0);i++)
+// {
+//     for (int j=0;j<newarray.GetLength(1);j++)
+//     {
 
-Console.WriteLine("Минимальное число: "+min);
-PrintArray(newarray);
+//         if (i == row ^ j ==colomn)
+//         newarray[i,j] = array[i+1,j+1];
+//         else
+//         newarray[i,j] = array[i,j];
+//     }
+// }
+
+// Console.WriteLine("Минимальное число: "+min);
+// PrintArray(newarray);
--- HomeWork/HomeWork8_Task54/Program.cs
+++ HomeWork/HomeWork8_Task54/Program.cs
@@ -24,26 +24,26 @@ void PrintArray(int[,] array)
     }
 }
 
-void RowSort (int[,] array, int n) // Сортирует элементы строки n в array по возрастанию
+void RowSort(int[,] array, int n) // Сортирует элементы строки n в array по возрастанию
 {
-    for (int i=0;i<array.GetLength(1);i++) //Проход по строке n
-    {   
-    int min = i;                            // Задаем индекс минимального элемента строки
-    for (int j=i;j<array.GetLength(1);j++)  // Ищем минимальный элемент в неотсорт. части
+    for (int i = 0; i < array.GetLength(1); i++) //Проход по строке n
     {
-        
-        if (array[n,j]<array[n,min])
+        int max = i;                            // Задаем индекс минимального элемента строки
+        for (int j = i; j < array.GetLength(1); j++)  // Ищем минимальный элемент в неотсорт. части
         {
-            min = j;                        // При нахождении нового мин. элемента
-        }                                   // заменяем индекс
-    }
 
-    if (array[n,min] == array[n,i])         // если мин элемент равен текущему - оставляем
-    continue;
+            if (array[n, j] > array[n, max])
+            {
+                max = j;                        // При нахождении нового мин. элемента
+            }                                   // заменяем индекс
+        }
 
-    int temp = array[n,i];                  // замена мин элмента на первый
-    array[n,i] = array[n,min];              // в не отсортированной части
-    array[n,min] = temp;
+        if (array[n, max] == array[n, i])         // если мин элемент равен текущему - оставляем
+            continue;
+
+        int temp = array[n, i];                  // замена мин элмента на первый
+        array[n, i] = array[n, max];              // в не отсортированной части
+        array[n, max] = temp;
     }
 }
 
@@ -52,9 +52,9 @@ int[,] array = new int[3, 4];
 FillArray(array);
 PrintArray(array);
 
-for (int i=0;i<array.GetLength(0);i++) // Проход по всем строкам array
+for (int i = 0; i < array.GetLength(0); i++) // Проход по всем строкам array
 {
-RowSort(array,i);
+    RowSort(array, i);
 }
 
 Console.WriteLine();

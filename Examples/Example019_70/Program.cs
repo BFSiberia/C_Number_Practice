9993264379c722f6158e76785627d75c7af5ad76
Ольга Смолина <a.v.tarasov@gmail.com>
2022-05-22 20:29:39 +0800

    add Example019_70

---
+++ Examples/Example019_70/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 70: Напишите программу, которая на вход принимает два числа и выдаёт
+// первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+// 3 и 4, N = 5 -> 3 4 7 11 18
+// 6 и 10, N = 4 -> 6 10 16 26
+
+int a=3;
+int b=4;
+int c=5;
+double SumDouble(int n)
+{
+    if (n==1) return a;
+    else if (n==2) return b;
+    else return SumDouble(n-1) + SumDouble(n-2);
+}
+
+for(int i=1;i<=c;i++)
+{
+    Console.Write(SumDouble(i)+" ");
+}
---
+++ Examples/Example019_72/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 72: Заданы 2 массива: info и data. В массиве info хранятся двоичные
+// представления нескольких чисел (без разделителя). В массиве data хранится
+// информация о количестве бит, которые занимают числа из массива info.
+// Напишите программу, которая составит массив десятичных представлений чисел
+// массива data с учётом информации из массива info.
+int[] array1 = { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
+int[] array2 = { 2, 3, 3, 1 };
+
+
+void Transfer(int[] numbers, int[] qty)
+{
+    int[] result = new int[qty.Length];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        for (int j = 0; j < qty[j]; j++)
+        {
+
+            result[i] = numbers[j];
+            Console.WriteLine(result[i] + " ");
+
+        }
+    }
+}
+
+Transfer(array1, array2);
+

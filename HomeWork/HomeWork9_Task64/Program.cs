304034b9227fc9c4574921f34910e47b09d18a84
Ольга Смолина <a.v.tarasov@gmail.com>
2022-05-22 17:41:10 +0800

    add HomeWork9_Task64

---
+++ HomeWork/HomeWork9_Task64/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от M до N.
+
+void NaturalNumber(int m, int n)
+{
+    if (m == n)
+    {
+        Console.Write(m);
+        return;
+    }
+
+
+    else if (m < n)
+    {
+        Console.Write(m + " ");
+        NaturalNumber(m + 1, n);
+    }
+    else
+    {
+        Console.Write(m + " ");
+        NaturalNumber(m - 1, n);
+    }
+}
+
+NaturalNumber(4, 8);
+Console.WriteLine();
+NaturalNumber(8, 4);
